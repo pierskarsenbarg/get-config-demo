551491560657945d4a3581d89970ce91f00a4f0b
Piers Karsenbarg <piers@pulumi.com>
2021-09-17 10:58:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Demo to get config from Pulumi console using Automation API
+
+## Instructions
+
+1. Clone repository: `git clone repoName`
+1. Install NPM packages: `npm install`
+1. Set the `organisationName`, `projectName` and `stackName` to match your
+1. Set some config values (`pulumi config set {name} {value}`)
+1. Run `npm run start`
---
+++ index.ts
@@ -0,0 +1,31 @@
+import { LocalWorkspace } from "@pulumi/pulumi/automation";
+
+
+// Set these values
+
+const organisationName = "";
+const projectName = "";
+const stackName = "";
+
+// You don't need to change anything below here
+
+(async () => {
+
+
+    // Create local workspace
+    const workspace = await LocalWorkspace.create({
+        projectSettings: {
+            name: projectName,
+            runtime: "nodejs",
+        }
+    });
+
+    // Refresh config from console
+    const config = await workspace.refreshConfig(`${organisationName}/${stackName}`);
+
+    // Loop through config key/value pairs
+    for (let configKey in config) {
+        console.log(`Config key: ${configKey} Config value: ${config[configKey].value}`);
+    }
+})();
+
